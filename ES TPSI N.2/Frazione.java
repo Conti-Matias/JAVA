c593112c260c0e8169aabe468b88f35dbab62849
Conti-Matias <113691220+Conti-Matias@users.noreply.github.com>
2024-02-12 09:31:39 +0100

    Update Frazione.java

--- ES TPSI N.2/Frazione.java
+++ ES TPSI N.2/Frazione.java
@@ -1,119 +1,116 @@
-public class Frazione {
+import java.util.Scanner;
+
+class Frazione {
     // Campi
     private int numeratore;
     private int denominatore;
     // Costruttore
 
-    public static void main(String[] args)
-    {
-        int x;
-        Frazione f1 = new Frazione(3, 5);
-        Frazione f2 = new Frazione(4, 7);
-
-        System.out.println("Frazione f1 = " + f1);
-        System.out.println("Frazione f2 = " + f2);
-
-        x = f1.getDenominatore();
-        System.out.println("Denominatore f1 = " + x);
-
-        x = f1.getNumeratore();
-        System.out.println("Numeratore f1 = " + x);
-
-        Frazione somma = f1.somma(f2);
-        System.out.println("Somma f1 + f2 = " + somma);
-
-        somma.semplifica();
-        System.out.println("Frazione somma semplificata = " + somma);
-
-        Frazione differenza = f1.sottrai(f2);
-        System.out.println("Differenza f1 - f2 = " + differenza);
-
-        Frazione prodotto = f1.moltiplica(f2);
-        System.out.println("Prodotto f1 * f2 = " + prodotto);
-
-        Frazione quoziente = f1.dividi(f2);
-        System.out.println("Quoziente f1 / f2 = " + quoziente);
-    }
-    
     public Frazione(int numeratore, int denominatore) {
         this.numeratore = numeratore;
         this.denominatore = denominatore;
     }
-    // Metodo per ottenere il numeratore
-    public int getNumeratore() {
-        return numeratore;
-    }
-// Metodo per ottenere il denominatore
-    public int getDenominatore() {
-        return denominatore;
-    }
-    // Metodo per ottenere la rappresentazione testuale della frazione
-    @Override
-    public String toString() {
-        return numeratore + "/" + denominatore;
-    }
-    // Metodo per sommare due frazioni
-    public Frazione somma(Frazione altraFrazione) {
-        int nuovoNumeratore = this.numeratore * altraFrazione.denominatore
-                + altraFrazione.numeratore * this.denominatore;
-        int nuovoDenominatore = this.denominatore * altraFrazione.denominatore;
 
-        return new Frazione(nuovoNumeratore, nuovoDenominatore);
-    }
+    public void semplifica(){
+       int div;
 
-    // Metodo per calcolare il Massimo Comune Divisore (MCD) con l'algoritmo di Euclide
-    private int calcolaMCD(int a, int b) 
-    {
-        int z, r;
-        if (a<b) {
-            z = a;
-            a = b;
-            b = z;
+       do{
+        div = calcolaMCD(numeratore, denominatore);
+        numeratore /= div;
+        denominatore /= div;
+       }
+       while(div != 1);
+    }
+        
+        public int calcolaMCD(int a, int b) {
+        int t,r;
+        if (a<b)
+        {
+            t = a;
+            a=b;
+            b=t;
         }
-
 
         do {
             r = a % b;
             if (r == 0) return b;
             a = b;
             b = r;
-        } while(r!=0);
+        }while(r!=0);
 
         return 0;
     }
 
-    // Metodo per semplificare la frazione
-    public void semplifica()
-    {
-        int mcd = calcolaMCD(this.numeratore, this.denominatore);
-        if(mcd != 0)
-        {
-        this.numeratore /= mcd;
-        this.denominatore /= mcd;
-        }
+
+    public Frazione moltiplica(Frazione altra) {
+        int newNumerator = this.numeratore * altra.numeratore;
+        int newDenominator = this.denominatore * altra.denominatore;
+        
+        // Semplifica la frazione se necessario
+        int MCD = calcolaMCD(newNumerator, newDenominator);
+        newNumerator /= MCD;
+        newDenominator /= MCD;
+        
+        return new Frazione(newNumerator, newDenominator);
     }
 
-    // Metodo per moltipicare la frazione
-    public Frazione moltiplica(Frazione altraFrazione)
-    {
-        int nuovoNumeratore = this.numeratore * altraFrazione.numeratore;
+    public Frazione sottrai(Frazione altraFrazione){
+        int nuovoNumeratore = this.numeratore * altraFrazione.denominatore
+                - altraFrazione.numeratore * this.denominatore;
         int nuovoDenominatore = this.denominatore * altraFrazione.denominatore;
+
+        return new Frazione(nuovoNumeratore, nuovoDenominatore);
+    }
+    
+
+    public Frazione dividi(Frazione altra){
+
+        int a;
+
+        a=altra.numeratore;
+        altra.numeratore=altra.denominatore;
+        altra.denominatore=a;
+
+
+        int nuovoNumeratore = this.numeratore * altra.numeratore;
+        int nuovoDenominatore = this.denominatore * altra.denominatore;      
+       
         return new Frazione(nuovoNumeratore, nuovoDenominatore);
     }
 
-    // Metodo per sottrarre due frazioni
-    public Frazione sottrai(Frazione altraFrazione)
-    {
-        int nuovoNumeratore = this.numeratore * altraFrazione.denominatore - altraFrazione.numeratore * this.denominatore;
-        int nuovoDenominatore = this.denominatore * altraFrazione.denominatore;
-        return new Frazione(nuovoNumeratore, nuovoDenominatore);
+    @Override
+    public String toString() {
+        return numeratore + "/" + denominatore;
     }
 
-    // Metodo per dividere due frazioni
-    public Frazione dividi(Frazione altraFrazione)
-    {
-        int nuovoNumeratore = this.numeratore * altraFrazione.denominatore;
-        int nuovoDenominatore = this.denominatore * altraFrazione.numeratore;
-        return new Frazione(nuovoNumeratore, nuovoDenominatore);
-    }
 }
+
+public class EsempioFrazione{
+
+    public static void main(String[] args) {
+        // Creazione di due frazioni
+        Frazione frazione1 = new Frazione(4, 6);
+        Frazione frazione2 = new Frazione(3, 4);
+
+        // Stampa delle frazioni
+        System.out.println("Frazione 1: " + frazione1);
+        System.out.println("Frazione 2: " + frazione2 + "\n");
+
+        //stampa semplifica
+        frazione1.semplifica();
+        System.out.println("Prima frazione semplificata: " + frazione1);
+        frazione2.semplifica();
+        System.out.println("Seconda frazione semplificata: " + frazione2+ "\n");
+
+        //moltiplica
+        Frazione moltiplica = frazione1.moltiplica(frazione2);
+        System.out.println("Frazione moltiplicata: " + moltiplica + "\n" );
+
+        //sottrai
+        Frazione sottrai = frazione1.sottrai(frazione2);
+        System.out.println("Sottrazione: " + sottrai + "\n");
+
+        Frazione dividi = frazione1.dividi(frazione2);
+        System.out.println("Divisione: " + dividi + "\n");
+    }
+}    
