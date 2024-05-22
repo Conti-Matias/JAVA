74383176ac3840af8a3f3bee8d1bbe532bb9b271
Conti-Matias <matimex06@gmail.com>
2024-05-22 11:15:48 +0000

    a

--- ES 4/A/A.js
+++ ES 4/A/A.js
@@ -1,28 +1,32 @@
-n1 = parseInt(prompt("inserisci il primo numero"))
-
-segno = prompt("inserisci un'operazione matematica tra +, -, /, *, **")
-
+n1 = parseInt(prompt("Inserisci il primo numero"))
 n2 = parseInt(prompt("inserisci il secondo numero"))
+segno = prompt("iserisci uno di questi segni per definire che operazione fare + - / * **")
 
-
-if(Number.isNaN(n1) || Number.isNaN(n2))
-{
-    alert("uno dei numeri inseriti non è valido")
+if(Number.isNaN(n1) || Number.isNaN(n2)){
+    alert("uno dei due numeri non e' un numero")
 }
-
-else if(segno == "+" || segno == "-" || segno == "/" || segno == "*" || segno == "**")
-{
-    funzioni = {
-        "+": (a,b) => a+b,
-        "-": (a,b) => a-b,
-        "/": (a,b) => a/b,
-        "*": (a,b) => a*b,
-        "**": (a,b) => a**b
+else{
+    switch(segno){
+        case "+":   const somma = function(a,b) {return a + b;};
+                    alert("il risultato e': " + somma(n1,n2))
+                    break
+        case "-":   const sottrazione = function(a,b) {return a - b;};
+                    alert("il risultato e': " + sottrazione(n1,n2))  
+                    break 
+  
+        case "/":   const divisione = function(a,b) {return a / b}
+                    alert("il risultato e':" + divisione(n1,n2))
+                    break
+          
+        case "*":   const moltiplicazione = function(a,b) {return a * b}
+                    alert("il risultato e':" + moltiplicazione(n1,n2))
+                    break
+  
+        case "**":  const potenza = function (a,b) {return a ** b}
+                    alert("il risultato e':" + potenza(n1,n2))
+                    break
+  
+        default: alert("segno inserito inesistente")
     }
 
-    alert(funzioni[segno](n1,n2))
-
-    
-} else {
-    alert("segno operazionale non valido")
 }
--- ES 4/B/B.js
+++ ES 4/B/B.js
@@ -1,7 +1,24 @@
-n = prompt("Inserisci un numero: ");
+alert("inserire un numero con almeno 2 cifre");
+numero = parseInt(prompt("quanti numeri desideri inserire"))
 
-function inverti(n) {
-    return n.split('').reverse().join('');
+if(Number.isNaN(numero)){
+  alert("numero inserito non valido")
 }
 
-alert(inverti(n));
+inverti(numero)
+
+function inverti(num){
+    
+    let reversed = 0;
+    while (num !== 0) {
+        
+        let digit = num % 10;
+      
+        reversed = reversed * 10 + digit;
+      
+        num = parseInt(num / 10);
+      }  
+
+      console.log(reversed);
+
+}
